c768d0e8290cd10f5ed0c3861bc54d01fab1205d
Dylan Ratcliffe <dylanratcliffe@outlook.com>
2021-06-02 20:02:56 +0100

    Refactored existing test data

--- tests/items.ts
+++ tests/items.ts
@@ -1,60 +1,52 @@
-import { Item, ItemAttributes, Reference } from '../items_pb';
+import { Item, ItemAttributes, Reference, Util } from '..';
 import { Struct } from "google-protobuf/google/protobuf/struct_pb";
 
-function newProcess(): Item {
-    const p = new Item();
-    const pAttributes = new ItemAttributes();
-
-    pAttributes.setAttrstruct(Struct.fromJavaScript({
+export const process = Util.newItem({
+    type: "process",
+    context: "myPod",
+    uniqueAttribute: "pid",
+    attributes: Util.newItemAttributes({
         "pid": 12323,
         "state": "running",
         "cpuPercent": 99.99,
-    }));
+    }),
+    linkedItemRequests: [],
+    linkedItems: [],
+    metadata: undefined,
+});
 
-    p.setAttributes(pAttributes);
-    p.setType("process");
-    p.setContext("myPod");
-    p.setUniqueattribute("pid");
+export const dylan = Util.newItem({
+    context: "global",
+    uniqueAttribute: "name",
+    type: "person",
+    attributes: Util.newItemAttributes({
+        "name": "dylan",
+        "age": 27,    
+    }),
+    linkedItemRequests: [],
+    linkedItems: [
+        Util.newReference({
+            context: "global",
+            type: "person",
+            uniqueAttributeValue: "katie",
+        }),
+    ],
+    metadata: undefined,
+});
 
-    return p;
-}
-
-export const dylan = new Item();
-export const katie = new Item();
-export const process = newProcess();
-const dylanAttributes = new ItemAttributes();
-const katieAttributes = new ItemAttributes();
-const dylanLink = new Reference();
-
-// Create and set attributes
-dylanAttributes.setAttrstruct(Struct.fromJavaScript({
-    "name": "dylan",
-    "age": 27,
-}))
-
-katieAttributes.setAttrstruct(Struct.fromJavaScript({
-    "name": "katie",
-    "age": 28,
-}))
-
-dylan.setAttributes(dylanAttributes)
-katie.setAttributes(katieAttributes)
-
-// Create a link between the two
-dylanLink.setType("person");
-dylanLink.setContext("global");
-dylanLink.setUniqueattributevalue("katie");
-
-dylan.addLinkeditems(dylanLink);
-
-// Set generic "person attributes"
-setPerson(dylan);
-setPerson(katie);
+export const katie = Util.newItem({
+    context: "global",
+    uniqueAttribute: "name",
+    type: "person",
+    attributes: Util.newItemAttributes({
+        "name": "katie",
+        "age": 28,
+        }),
+    linkedItemRequests: [],
+    linkedItems: [],
+    metadata: undefined,
+});
 
 export const items = [dylan, katie];
 
-function setPerson(p: Item): void {
-    p.setType("person");
-    p.setUniqueattribute("name");
-    p.setContext("global");
-}
+
