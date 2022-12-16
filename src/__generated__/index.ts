4ae610ff92b9ba96ebd0fb8dc4f9f7e2703acf1f
Elliot Waddington <getinnocuous@users.noreply.github.com>
2022-12-16 12:19:24 +0000

    update exports

--- src/__generated__/index.ts
+++ src/__generated__/index.ts
@@ -1,3 +1,3 @@
-export * from './gateway_pb'
-export * from './items_pb'
-export * from './responses_pb'
+export { ItemRequest, ItemAttributes, Item, Items, Edge, Reference, Metadata, RequestMethod, CancelItemRequest, ReverseLinksRequest, ReverseLinksResponse } from './items_pb';
+export { Response,ItemRequestError, ResponderState } from './responses_pb';
+export { GatewayRequest, GatewayRequestStatus, GatewayResponse } from './gateway_pb'
