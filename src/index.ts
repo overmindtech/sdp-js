9a20ffd4f51111e5c4b36c78d0f45adac15d9ad9
Elliot Waddington <getinnocuous@users.noreply.github.com>
2022-12-23 16:34:38 +0000

    fix: copy existing d.ts to build folder (#24)

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,19 @@ export * from './GatewaySession'
 export * from './RequestProgress'
 export * from './Responder'
 export * from './Util'
+export {
+  CancelItemRequest,
+  Edge,
+  GatewayRequest,
+  GatewayRequestStatus,
+  GatewayResponse,
+  Item,
+  ItemAttributes,
+  ItemRequest,
+  ItemRequestError,
+  Metadata,
+  Reference,
+  RequestMethod,
+  ResponderState,
+  Response,
+} from './__generated__/'
