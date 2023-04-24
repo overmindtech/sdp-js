df362841556b4f70496b7239e2f0b819399d7fb9
Elliot Waddington <elliot_waddington@hotmail.co.uk>
2023-04-24 12:07:32 +0100

    refactor: export types

--- src/protobuf.ts
+++ src/protobuf.ts
@@ -1,70 +1,10 @@
-// Types
-export {
-  Bookmark,
-  BookmarkProperties,
-  BookmarkMetadata,
-  ListBookmarksRequest,
-  ListBookmarkResponse,
-  CreateBookmarkRequest,
-  CreateBookmarkResponse,
-  GetBookmarkRequest,
-  GetBookmarkResponse,
-  UpdateBookmarkRequest,
-  UpdateBookmarkResponse,
-  DeleteBookmarkRequest,
-  DeleteBookmarkResponse,
-} from './__generated__/bookmarks_pb'
-export {
-  GatewayRequest,
-  GatewayResponse,
-  GatewayRequestStatus,
-  GatewayRequestStatus_Summary,
-  StoreBookmark,
-  BookmarkStoreResult,
-  LoadBookmark,
-  BookmarkLoadResult,
-  StoreSnapshot,
-  SnapshotStoreResult,
-  LoadSnapshot,
-  SnapshotLoadResult,
-} from './__generated__/gateway_pb'
-export {
-  Health,
-  QueryMethod,
-  Item,
-  ItemAttributes,
-  Metadata,
-  Items,
-  Query,
-  QueryStatus,
-  QueryStatus_Status,
-  QueryError,
-  QueryError_ErrorType,
-  CancelQuery,
-  UndoQuery,
-  Expand,
-  UndoExpand,
-  Reference,
-  Edge,
-  ReverseLinksRequest,
-  ReverseLinksResponse,
-} from './__generated__/items_pb'
-export { ResponderState, Response } from './__generated__/responses_pb'
-export {
-  Snapshot,
-  SnapshotProperties,
-  SnapshotMetadata,
-  ListSnapshotsRequest,
-  ListSnapshotResponse,
-  CreateSnapshotRequest,
-  CreateSnapshotResponse,
-  GetSnapshotRequest,
-  GetSnapshotResponse,
-  UpdateSnapshotRequest,
-  UpdateSnapshotResponse,
-  DeleteSnapshotRequest,
-  DeleteSnapshotResponse,
-} from './__generated__/snapshots_pb'
+// Protobuf
+export * from './__generated__/bookmarks_pb'
+export * from './__generated__/changes_pb'
+export * from './__generated__/gateway_pb'
+export * from './__generated__/items_pb'
+export * from './__generated__/responses_pb'
+export * from './__generated__/snapshots_pb'
 
 // Services
 export { BookmarksService } from './__generated__/bookmarks_connect'
