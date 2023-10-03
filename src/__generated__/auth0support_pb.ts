7d73aa4bea55507c7ec938d392dde52b2af9a8f1
Engineering <github-action@users.noreply.github.com>
2023-10-03 14:07:50 +0000

    Updated to SDP v0.18.4-149-g58eebbe

--- src/__generated__/auth0support_pb.ts
+++ src/__generated__/auth0support_pb.ts
@@ -20,6 +20,11 @@ export class Auth0CreateUserRequest extends Message<Auth0CreateUserRequest> {
    */
   email = "";
 
+  /**
+   * @generated from field: string name = 3;
+   */
+  name = "";
+
   constructor(data?: PartialMessage<Auth0CreateUserRequest>) {
     super();
     proto3.util.initPartial(data, this);
@@ -30,6 +35,7 @@ export class Auth0CreateUserRequest extends Message<Auth0CreateUserRequest> {
   static readonly fields: FieldList = proto3.util.newFieldList(() => [
     { no: 1, name: "user_id", kind: "scalar", T: 9 /* ScalarType.STRING */ },
     { no: 2, name: "email", kind: "scalar", T: 9 /* ScalarType.STRING */ },
+    { no: 3, name: "name", kind: "scalar", T: 9 /* ScalarType.STRING */ },
   ]);
 
   static fromBinary(bytes: Uint8Array, options?: Partial<BinaryReadOptions>): Auth0CreateUserRequest {
