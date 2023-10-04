5c7044076f6ec2d3eba0de2528a1508a14e15370
David Schmitt <118179693+DavidS-om@users.noreply.github.com>
2023-10-04 09:14:02 +0200

    re-export new services (#134)

--- src/services.ts
+++ src/services.ts
@@ -1,5 +1,8 @@
 // Services
+export * from './__generated__/account_connect'
+export * from './__generated__/apikey_connect'
+export * from './__generated__/auth0support_connect'
 export * from './__generated__/bookmarks_connect'
 export * from './__generated__/changes_connect'
+export * from './__generated__/invites_connect'
 export * from './__generated__/snapshots_connect'
-export * from './__generated__/apikey_connect'
