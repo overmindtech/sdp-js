f9c1e6cafb0af5b91a36fb10b1883ebbcd8c54f7
Elliot Waddington <getinnocuous@users.noreply.github.com>
2024-12-17 15:25:50 +0100

    feat: export auto tagging service

--- src/connect.ts
+++ src/connect.ts
@@ -1,82 +1,9 @@
 // Connect
-export {
-  listBookmarks,
-  createBookmark,
-  getBookmark,
-  updateBookmark,
-  getAffectedBookmarks,
-  deleteBookmark,
-} from './__generated__/bookmarks-BookmarksService_connectquery'
-export {
-  listApps,
-  createApp,
-  createSimpleApp,
-  getApp,
-  updateApp,
-  deleteApp,
-  listChanges,
-  createChange,
-  refreshState,
-  getChange,
-  updateChange,
-  deleteChange,
-  getOnboarding,
-  updateOnboarding,
-  listHomeApps,
-  listHomeChanges,
-  getAppSummary,
-  listAppChanges,
-  listAppChangesSummary,
-  getAffectedApps,
-  listChangingItemsSummary,
-  getDiff,
-  getChangeTimeline,
-  getAppSummaries,
-  getChangeRisks,
-  getChangeArchive,
-  listChangesByStatus,
-  populateChangeFilters,
-} from './__generated__/changes-ChangesService_connectquery'
-export {
-  listSnapshots,
-  createSnapshot,
-  getSnapshot,
-  updateSnapshot,
-  deleteSnapshot,
-} from './__generated__/snapshots-SnapshotsService_connectquery'
-export {
-  createAPIKey,
-  refreshAPIKey,
-  getAPIKey,
-  updateAPIKey,
-  listAPIKeys,
-  deleteAPIKey,
-  exchangeKeyForToken,
-} from './__generated__/apikeys-ApiKeyService_connectquery'
-export {
-  getAccount,
-  listSources,
-  createSource,
-  getSource,
-  updateSource,
-  deleteSource,
-  keepaliveSources,
-  listAllSourcesStatus,
-  createToken,
-  deleteAccount,
-  getTrialEnd,
-  listAvailableItemTypes,
-} from './__generated__/account-ManagementService_connectquery'
-export {
-  listInvites,
-  createInvite,
-  revokeInvite,
-  resendInvite,
-} from './__generated__/invites-InviteService_connectquery'
-export {
-  getAccountConfig,
-  updateAccountConfig,
-  createHcpConfig,
-  getHcpConfig,
-  deleteHcpConfig,
-} from './__generated__/config-ConfigurationService_connectquery'
+export * from './__generated__/bookmarks-BookmarksService_connectquery'
+export * from './__generated__/changes-ChangesService_connectquery'
+export * from './__generated__/snapshots-SnapshotsService_connectquery'
+export * from './__generated__/apikeys-ApiKeyService_connectquery'
+export * from './__generated__/account-ManagementService_connectquery'
+export * from './__generated__/invites-InviteService_connectquery'
+export * from './__generated__/config-ConfigurationService_connectquery'
+export * from './__generated__/changes-AutoTaggingService_connectquery'
